9a69cbfed48696c65c585be50437284741fbec86
Rafael Evangelista <rafaelhugo03@outlook.com>
2023-05-05 11:13:44 -0300

    Create mappers for order and order item

--- Marketplace.Application/AutoMapper/DomainToViewModelMapper.cs
+++ Marketplace.Application/AutoMapper/DomainToViewModelMapper.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using Marketplace.Application.Models;
-using Marketplace.Domain.Entities;
 using Marketplace.Domain.Entities;
 
 namespace Marketplace.Application.AutoMapper
@@ -15,6 +10,8 @@ namespace Marketplace.Application.AutoMapper
         {
             CreateMap<User, UserDTO>();
             CreateMap<Product, ProductDTO>();
+            CreateMap<OrderItem, OrderItemDTO>();
+            CreateMap<Order, OrderDTO>();
         }
     }
 }
--- Marketplace.Application/AutoMapper/ViewModelToDomainMapper.cs
+++ Marketplace.Application/AutoMapper/ViewModelToDomainMapper.cs
@@ -7,6 +7,9 @@ using Marketplace.Application.Models;
 using Marketplace.Domain.Entities;
 using Marketplace.Domain.Commands.ProductCommands;
 using Marketplace.Domain.Commands.UserCommands;
+using Marketplace.Domain.Commands.OrderCommands;
+using Marketplace.Application.Utils;
+using Marketplace.Domain.Commands.OrderItemCommands;
 
 namespace Marketplace.Application.AutoMapper
 {
@@ -18,7 +21,7 @@ namespace Marketplace.Application.AutoMapper
                 .ConstructUsing(c => new RegisterUserCommand(
                     c.Name, 
                     c.EmailAddress,  
-                    c.Password,
+                    c.Password.StringHasher(),
                     c.BirthDate));
 
             CreateMap<UserDTO, UpdateUserCommand>()
@@ -28,6 +31,11 @@ namespace Marketplace.Application.AutoMapper
                     c.EmailAddress, 
                     c.Password,
                     c.BirthDate));
+
+            CreateMap<UserDTO, DeleteUserCommand>()
+                .ConstructUsing(c => new DeleteUserCommand(
+                    c.Id
+                ));
 
             CreateMap<ProductDTO, RegisterProductCommand>()
                 .ConstructUsing(c => new RegisterProductCommand(
@@ -45,6 +53,36 @@ namespace Marketplace.Application.AutoMapper
                     c.Description,
                     c.Price,
                     c.Quantity
+                ));
+
+            CreateMap<ProductDTO, DeleteProductCommand>()
+                .ConstructUsing(c => new DeleteProductCommand(
+                    c.Id
+                ));
+
+            CreateMap<OrderDTO, RegisterOrderCommand>()
+                .ConstructUsing(c => new RegisterOrderCommand(
+                    c.TotalPrice,
+                    c.UserBuyerId,
+                    c.OrderItems.Select(s => s.ToCommand()).ToList()
+                ));
+
+            CreateMap<OrderItemDTO, RegisterOrderItemCommand>()
+                .ConstructUsing(c => new RegisterOrderItemCommand(
+                    c.Id,
+                    c.ProductId,
+                    c.Quantity,
+                    c.Price
+                ));
+
+            CreateMap<OrderDTO, CancelOrderCommand>()
+                .ConstructUsing(c => new CancelOrderCommand(
+                    c.Id
+                ));
+
+            CreateMap<OrderDTO, PayOrderCommand>()
+                .ConstructUsing(c => new PayOrderCommand(
+                    c.Id
                 ));
         }
     }
