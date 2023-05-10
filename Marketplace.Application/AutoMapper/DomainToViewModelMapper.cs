fe23e588204a15207266de6d1e87206431866bf3
Rafael Evangelista <rafaelhugo03@outlook.com>
2023-05-10 18:42:50 -0300

    Fix mapper

--- Marketplace.Application/AutoMapper/DomainToViewModelMapper.cs
+++ Marketplace.Application/AutoMapper/DomainToViewModelMapper.cs
@@ -8,7 +8,7 @@ namespace Marketplace.Application.AutoMapper
     {
         public DomainToViewModelMapper()
         {
-            CreateMap<User, UserDTO>().AfterMap((src, dest) => dest.Password = "");
+            CreateMap<User, UserDTO>().AfterMap((src, dest) => dest.Password = null);
             CreateMap<Product, ProductDTO>();
             CreateMap<OrderItem, OrderItemDTO>();
             CreateMap<Order, OrderDTO>();
