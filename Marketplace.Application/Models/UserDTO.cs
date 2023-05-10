f583a75eaafd63aa572044b944361efda084cbd4
Rafael Evangelista <rafaelhugo03@outlook.com>
2023-05-10 18:43:47 -0300

    Add role in dto

--- Marketplace.Application/Models/UserDTO.cs
+++ Marketplace.Application/Models/UserDTO.cs
@@ -1,3 +1,5 @@
+using Marketplace.Domain.enums;
+
 namespace Marketplace.Application.Models
 {
     public class UserDTO
@@ -5,7 +7,8 @@ namespace Marketplace.Application.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string EmailAddress { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
         public DateTime BirthDate { get; set; }
+        public Role Role { get; set; }
     }
 }
