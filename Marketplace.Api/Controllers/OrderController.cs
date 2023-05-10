3592af78cc04a1a43b654288beb931c3d643874b
Rafael Evangelista <rafaelhugo03@outlook.com>
2023-05-10 18:45:21 -0300

    add authorization in endpoints

--- Marketplace.Api/Controllers/OrderController.cs
+++ Marketplace.Api/Controllers/OrderController.cs
@@ -8,10 +8,13 @@ namespace Marketplace.Api.Controllers
     public class OrderController : ApiController
     {
         private readonly IOrderService orderService;
+        private readonly ITokenService tokenService;
 
-        public OrderController(IOrderService orderService)
+        public OrderController(IOrderService orderService,
+            ITokenService tokenService)
         {
             this.orderService = orderService;
+            this.tokenService = tokenService;
         }
 
         [HttpGet("order-management/get-all")]
@@ -21,30 +24,36 @@ namespace Marketplace.Api.Controllers
         }
 
         [HttpGet("order-management/get-all/{userBuyerId:Guid}")]
+        [Authorize]
         public async Task<IActionResult> GetAllByBuyer(Guid userBuyerId)
         {
             return CustomResponse(await orderService.GetAllByBuyer(userBuyerId));
         }
 
         [HttpGet("order-management/get-all/{id:Guid}")]
+        [Authorize]
         public async Task<IActionResult> GetById(Guid id)
         {
             return CustomResponse(await orderService.GetById(id));
         }
 
         [HttpPost("order-management/create")]
+        [Authorize]
         public async Task<IActionResult> Create([FromBody] OrderDTO dto)
         {
+            dto.UserBuyerId = tokenService.GetIdInToken(Request);
             return CustomResponse(await orderService.Register(dto));
         }
 
         [HttpPut("order-management/cancel-order")]
+        [Authorize]
         public async Task<IActionResult> CancelOrder([FromBody] OrderDTO dto)
         {
             return CustomResponse(await orderService.Cancelorder(dto));
         }
 
         [HttpPut("order-management/pay-order")]
+        [Authorize]
         public async Task<IActionResult> PayOrder([FromBody] OrderDTO dto)
         {
             return CustomResponse(await orderService.PayOrder(dto));
